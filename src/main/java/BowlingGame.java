2b0db154ea8ae5c091ad8724db7d8c82f6431c87
micaela berry <msberry9@gmail.com>
2017-08-29 15:06:04 -0400

    final coding part

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,32 +1,53 @@
 
 public class BowlingGame {
 
-	private int rolls[] = new int[21];
-	private int currentRoll = 0;
 
-	public void roll(int pins) {
-		rolls[currentRoll++] = pins;
-	}
+		private int rolls[] = new int[21];
+		private int currentRoll = 0;
 
-	public int score() {
-		int score = 0;
-		int index = 0;
-		for (int frame = 0; frame < 10; frame++) {
-			if (rolls[index] == 10) {
-				score += 10 + rolls[index + 2];
-			} else if (isSpare(index)) {
-				score += 10 + rolls[index + 1];
-				index += 2;
-			} else {
-				score += rolls[index] + rolls[index + 1];
-				index += 2;
-			}
+		public void roll(int pins) {
+			// remember, this is the same as..
+			// rolls[currentRoll] = pins;
+			// currentRoll++;
+			rolls[currentRoll++] = pins;
 		}
-		return score;
+
+		public int score() {
+			int score = 0;
+			int index = 0;
+			for (int frame = 0; frame < 10; frame++) {
+				if (isStrike(index)) {
+					score += 10 + strikeBonus(index);
+					index++;
+				} else if (isSpare(index)) {
+					score += 10 + spareBonus(index);
+					index += 2;
+				} else {
+					score += ballsInFrame(index);
+					index += 2;
+				}
+			}
+			return score;
+		}
+
+		private boolean isStrike(int index) {
+			return rolls[index] == 10;
+		}
+
+		private boolean isSpare(int index) {
+			return ballsInFrame(index) == 10;
+		}
+
+		private int strikeBonus(int index) {
+			return rolls[index + 1] + rolls[index + 2];
+		}
+
+		private int spareBonus(int index) {
+			return rolls[index + 2];
+		}
+
+		private int ballsInFrame(int index) {
+			return rolls[index] + rolls[index + 1];
+		}
 
 	}
-
-	private boolean isSpare(int index) {
-		return rolls[index] + rolls[index + 1] == 10;
-	}
-}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,60 +1,60 @@
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
 	private BowlingGame g;
-	
-	@Before
-	public void setUp(){
-		g = new BowlingGame();
-	}
 
-	private void rollMany(int numberOfTimes, int pins) { //this is the extracted method rollMany
-		for (int i = 0; i < numberOfTimes; i++) { // rolling the ball 20 times method
-			g.roll(pins); // write a for loop in the test so you don't have to write it 20 times in the class 
-		}
+	@Before
+	public void setup() {
+		g = new BowlingGame();
 	}
 	
 	@Test
 	public void shouldScoreGutterGame() {
-		rollMany(20, 0); //right click extract method and extract local variable 
-		// act
+		rollMany(20, 0);
 		int score = g.score();
-		
-		//assert
-		assertEquals("should be zero score- all gutterballs!", 0, score); //(expected is 0 and actual is score
-		assertThat("should be zero score- all gutterballs!", score, CoreMatchers.is(0)); //actual is score and expected is 0
+		assertThat("should be zero score - all gutterballs!", score, is(0));
+		assertEquals("should be zero score - all gutterballs!", 0, score);
 	}
 
+	private void rollMany(int numberOfTimes, int pins) {
+		for(int i = 0; i < numberOfTimes; i++) {
+			g.roll(pins);
+		}
+	}
 	
 	@Test
 	public void shouldScoreAllOnes() {
-		rollMany(20,1);
+		rollMany(20, 1);
 		int score = g.score();
-		assertEquals("should be one score", 20, score);
-		
-}
-	@Test
-	public void shouldScoreOneSpare(){
-		g.roll(5);
-		g.roll(5);
-		g.roll(3);
-		rollMany(17,0);
-		int score = g.score();
-		assertThat(score, CoreMatchers.is(16));
+		assertThat(score, is(20));
 	}
 	
 	@Test
-	public void shouldScoreOneStrike(){
-		g.roll(10);
+	public void shouldScoreOneSpare() {
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		int score = g.score();
+		assertThat(score, is(16));
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+	
+	@Test
+	public void shouldScoreOneStrike() {
+		g.roll(10); // strike!
 		g.roll(4);
 		g.roll(3);
 		int score = g.score();
-		assertThat(score, CoreMatchers.is(24));
+		assertThat(score, is(24));
 	}
 }
-
