23d74c4eef71f92520c0127a3a38965132a3302b
micaela berry <msberry9@gmail.com>
2017-08-29 14:38:00 -0400

    new tests

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,13 +1,19 @@
 
 public class BowlingGame {
 
-	int score = 0;
+	private int rolls []= new int [21];
+	private int currentRoll = 0;
 	
 	public void roll(int pins) {
 		score += pins;
+		rolls[currentRoll++] = pins ;
 	}
 
 	public int score() {
+		int score = 0;
+		for (int roll: rolls){
+			score += roll;
+		}
 		return score;
 	}
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -13,6 +13,12 @@ public class BowlingGameTest {
 		g = new BowlingGame();
 	}
 
+	private void rollMany(int numberOfTimes, int pins) { //this is the extracted method rollMany
+		for (int i = 0; i < numberOfTimes; i++) { // rolling the ball 20 times method
+			g.roll(pins); // write a for loop in the test so you don't have to write it 20 times in the class 
+		}
+	}
+	
 	@Test
 	public void shouldScoreGutterGame() {
 		rollMany(20, 0); //right click extract method and extract local variable 
@@ -24,19 +30,22 @@ public class BowlingGameTest {
 		assertThat("should be zero score- all gutterballs!", score, CoreMatchers.is(0)); //actual is score and expected is 0
 	}
 
-	private void rollMany(int numberOfTimes, int pins) { //this is the extracted method rollMany
-		for (int i = 0; i < numberOfTimes; i++) { // rolling the ball 20 times method
-			g.roll(pins); // write a for loop in the test so you don't have to write it 20 times in the class 
-		}
-	}
 	
 	@Test
 	public void shouldScoreAllOnes() {
-		BowlingGame g = new BowlingGame();
-		rollMany(20, 1);
+		rollMany(20,1);
 		int score = g.score();
-		
 		assertEquals("should be one score", 0, score); 
+		
+}
+//	@Test
+//	public void shouldScoreOneSpare(){
+//		g.roll(5);
+//		g.roll(5);
+//		g.roll(3);
+//		rollMany(17,0);
+//		int score = g.score();
+//		assertThat(score, CoreMatchers.is(16));
+//	}
+}
 
-}
-}
